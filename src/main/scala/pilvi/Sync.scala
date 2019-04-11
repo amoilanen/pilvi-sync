cdcbdcdba9a275068eb55f4934e0ebf92acec283
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-04-11 23:50:37 +0300

    Ensure the ordering of asynchronous operations relative to each other

--- src/main/scala/pilvi/Sync.scala
+++ src/main/scala/pilvi/Sync.scala
@@ -14,7 +14,7 @@ object Sync extends App {
 
   val drive: GoogleDrive = GoogleDrive.getDrive
 
-  def upload(localDirectoryPath: String, remoteDirectoryPath: String): Unit = {
+  def upload(localDirectoryPath: String, remoteDirectoryPath: String): Future[Unit] = {
     println(s"Uploading ${localDirectoryPath} to ${remoteDirectoryPath}")
     val localFilesAndDirectories: List[LocalFile] = LocalDrive.listFiles(localDirectoryPath)
 
@@ -25,34 +25,32 @@ object Sync extends App {
     //println("Local dirs = ")
     //println(localDirectories)
 
-    drive.ensureExists(FilePath(remoteDirectoryPath))
-
-    localDirectories.map(localDirectory => {
-      val directoryName = localDirectory.getName
-      upload(s"${localDirectoryPath}/${directoryName}", s"${remoteDirectoryPath}/${directoryName}")
-    })
-
-    val fileUploads: Seq[Future[Option[FileId]]] = localFiles.map(file => {
-      val relativeFileName = file.getName
-      if (!Await.result(drive.exists(FilePath(s"${remoteDirectoryPath}/${relativeFileName}")), timeout)) {
-        println(s"${relativeFileName} uploading again")
-        drive.uploadFile(FilePath(remoteDirectoryPath), file).map(Some(_))
-      } else {
-        println(s"${relativeFileName} already exists")
-        Future { None }
-      }
-    })
-
-    val fileIds = Await.ready(Future.sequence(fileUploads), timeout)
-    println(fileIds)
+    drive.ensureExists(FilePath(remoteDirectoryPath)).flatMap { _ =>
+      Future.sequence(localDirectories.map(localDirectory => {
+        val directoryName = localDirectory.getName
+        upload(s"${localDirectoryPath}/${directoryName}", s"${remoteDirectoryPath}/${directoryName}")
+      }))
+    }.flatMap { _ =>
+      val fileUploads: Seq[Future[Option[FileId]]] = localFiles.map(file => {
+        val relativeFileName = file.getName
+        if (!Await.result(drive.exists(FilePath(s"${remoteDirectoryPath}/${relativeFileName}")), timeout)) {
+          println(s"${relativeFileName} uploading again")
+          drive.uploadFile(FilePath(remoteDirectoryPath), file).map(Some(_))
+        } else {
+          println(s"${relativeFileName} already exists")
+          Future { None }
+        }
+      })
+      Future.sequence(fileUploads)
+    }.map {_ => ()}
   }
-
-  val files = Await.result(drive.listFiles(FilePath("/")), timeout)
-
-  files.foreach(file => println(file.getName))
 
   val localSyncFolderPath = "src/main/resources/test"
   val remoteFolderPath = "pilvi-sync"
 
-  upload(localSyncFolderPath, remoteFolderPath)
+  Await.result(upload(localSyncFolderPath, remoteFolderPath), 10 minutes)
+
+  //TODO: Add logger and log the upload progress
+  val files = Await.result(drive.listFiles(FilePath("/pilvi-sync")), timeout)
+  files.foreach(file => println(file.getName))
 }
